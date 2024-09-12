2a5a35ad06c4ccff8cec040bfd295630d72e39ea
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2024-09-12 11:40:13 -0500

    fuzz test From impls

--- src/from.rs
+++ src/from.rs
@@ -14,3 +14,49 @@ impl From<Int64_9> for Int128_18 {
         Decimal((value.0 as i128) * 10i128.pow(9))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use proptest::prelude::Arbitrary;
+    use proptest::test_runner::TestRunner;
+
+    use super::*;
+
+    #[test]
+    fn uint128_18_from_uint64_9() {
+        let mut runner = TestRunner::default();
+        let input = Decimal::arbitrary();
+
+        runner
+            .run(&input, |decimal: Decimal<u64, 9>| {
+                let out = Uint128_18::from(decimal);
+                let out_f = f64::from_str(&out.to_string()).unwrap();
+                let decimal_f = f64::from_str(&decimal.to_string()).unwrap();
+
+                assert_eq!(out_f, decimal_f);
+
+                Ok(())
+            })
+            .unwrap();
+    }
+
+    #[test]
+    fn int128_18_from_int64_9() {
+        let mut runner = TestRunner::default();
+        let input = Decimal::arbitrary();
+
+        runner
+            .run(&input, |decimal: Decimal<i64, 9>| {
+                let out = Int128_18::from(decimal);
+                let out_f = f64::from_str(&out.to_string()).unwrap();
+                let decimal_f = f64::from_str(&decimal.to_string()).unwrap();
+
+                assert_eq!(out_f, decimal_f);
+
+                Ok(())
+            })
+            .unwrap();
+    }
+}
