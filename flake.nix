edd099410b4f2d3e25ffc44ea31578b1a3709dde
MathisWellmann <wellmannmathis@gmail.com>
2025-12-09 15:00:23 +0000

    nix: update flake with more features

--- flake.nix
+++ flake.nix
@@ -1,23 +1,43 @@
-# Based on: https://crane.dev/examples/quick-start-workspace.html
-
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
 
-  outputs =
-    {
-      nixpkgs,
-      ...
-    }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      # Dev.
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          rustPlatform.bindgenHook
-        ];
-      };
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    ...
+  }: let
+    system = "x86_64-linux";
+    overlays = [(import rust-overlay)];
+    pkgs = import nixpkgs {
+      inherit system overlays;
     };
+    rustToolchain = pkgs.rust-bin.stable.latest.minimal.override {
+      extensions = [
+        "rust-src"
+        "clippy"
+        "rust-analyzer"
+      ];
+    };
+
+    buildInputs = with pkgs; [
+      rustPlatform.bindgenHook
+      rustToolchain
+      rust-bin.nightly.latest.rustfmt # Use nightly formatter which has some better features.
+      cargo-nextest
+      taplo # Toml toolkit for formatting `Cargo.toml`.
+    ];
+    nix_tools = with pkgs; [
+      alejandra # Formatter for nix code. `alejandra .`
+      deadnix # Check for dead nix code.
+      statix # Code checker.
+    ];
+  in {
+    # Dev.
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = buildInputs ++ nix_tools;
+    };
+  };
 }
